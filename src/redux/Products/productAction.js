e2fcd00f1fda956479c759deb3fb6e55bc52819b
snehalpatil <snehalpro55@gmail.com>
2020-03-26 17:31:42 +0530

    added middleware

--- src/redux/Products/productAction.js
+++ src/redux/Products/productAction.js
@@ -1,7 +1,9 @@
 import { GET_PRODUCT } from './productType'
 
 export const getProduct = () =>{
+    console.log('in action')
     return {
+        
         type : GET_PRODUCT
     }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import ProductReducer from './Products/ProductReduser'
+import thunk from 'redux-thunk'
+// import applyMiddleware from 'redux-thunk'
 
-const store = createStore(ProductReducer)
+
+const store = createStore(ProductReducer,applyMiddleware(thunk))
 
 export default store
