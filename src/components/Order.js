ddfbfdda123594a700b6936f2787cd701a7ad5bc
snehalpatil <snehalpro55@gmail.com>
2020-04-13 19:04:43 +0530

    created and added product details component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import Footer from './footer'
 import HomeNavBar from './navbar'
-import { Container,Row,Col } from 'react-bootstrap'
+// import { Container,Row,Col } from 'react-bootstrap'
 import '../assets/css/Order.css'
 import StarRatingComponent from 'react-star-rating-component';
 
 // import ProductCard from './ProductCard'
 
 export default class Order extends React.Component{
-    hello(){
-        // return(<Footer/>)
-        alert("welcome");
-        
-    }
+   
     render(){
         return(
-            <div></div>
+            <div>
+                <HomeNavBar/>
+                <br></br>
+                <Footer/>
+            </div>
         );
     }
 }
