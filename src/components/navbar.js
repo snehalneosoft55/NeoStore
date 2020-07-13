95e47314606967ebcef811b1e409405785268ab8
snehalneosoft55 <patil.snehal@neosofttech.com>
2020-07-13 18:37:55 +0530

    search module

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,28 +11,110 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import LoginReg from "./LoginReg";
 import LogProfile from "./LogProfile";
-import {withRouter} from 'react-router-dom'
-let x=''
+import { withRouter } from "react-router-dom";
+import { getProducts } from "../actions/displayProductAction";
+import {connect} from 'react-redux'
+
+
+let x = "";
 class HomeNavbar extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state={
+      search:null,
+      Information:''
     }
-    checkForLogIn=()=>{
-        const token = localStorage.getItem('token');
-        if(token ===null){
-            alert("Please lLogin first");
-            this.props.history.push('/signIn');
-        }
-        else{
-            this.props.history.push({
-                pathname:'Order',
-                state:{
-                    show:0
-                }
-            });
-        }
+  }
+  componentWillMount(){
+    this.props.getProducts().then(() => {
+      const { productData } = this.props;
+      console.log("productdata in didmount in nav", productData.productData);
+      const x = productData.productData;
+      console.log("Information",x);
+      this.setState({ Information:x});
+    });
+  }
+  searchSpace=(event)=>{
+    console.log("keyword==");
+    let keyword = event.target.value;
+    console.log("keyword==",keyword);
+    this.setState({search:keyword})
+  }
+
+  checkForLogIn = () => {
+    const token = localStorage.getItem("token");
+    if (token === null) {
+      alert("Please lLogin first");
+      this.props.history.push("/signIn");
+    } else {
+      this.props.history.push({
+        pathname: "Order",
+        state: {
+          show: 0,
+          
+        },
+      });
     }
+  };
   render() {
+    const Informations =this.state.Information;
+    console.log("Informations-----",Informations);
+    let items;
+    let productList;
+    if(Informations ===undefined || Informations === '' ){
+      items='';
+    }
+    else{
+    
+      items = Informations.filter((data)=>{
+        console.log("data==and==data.product_name",data,data.product_name);
+        console.log("data.product_name",data.product_name);
+        console.log("this.state.search",this.state.search);
+        if(this.state.search == '' || this.state.search == null)
+            return data.product_name
+        else if(data.product_name.toLowerCase().includes(this.state.search.toLowerCase())){
+          console.log("data in else if==",data);
+            return data.product_name
+        }
+      }).map((data,i)=>{
+        console.log("data in map==",data);
+        // productList=data.product_name;
+        return(
+        <div>
+          <ul>
+            <li style={{position:'relative',left:'10vh'}}>
+              <span >{data.product_name}</span>
+            </li>
+          </ul>
+        </div>
+        )
+      })
+    }
+    console.log("items=======",items);
+
+    // if(Informations !== undefined && this.state.search !== ''){
+    //   console.log("in==",Informations);
+    //   let items;
+    //   items = Informations.filter((data)=>{
+    //     console.log("data=== in filter",data);
+    //     if(this.state.search == null)
+    //         return data
+    //     else if(data.color_id.product_name.toLowerCase().includes(this.state.search.toLowerCase())){
+    //         return data
+    //     }
+    //   }).map(data=>{
+    //     return(
+    //     <div>
+    //       <ul>
+    //         <li style={{position:'relative',left:'10vh'}}>
+    //           <span >{data.color_id.product_name}</span>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     )
+    //   })
+    // }
+    
     return (
       <div>
         <Navbar bg="dark" className="homeNav" expand="lg" variant="dark">
@@ -67,29 +149,25 @@ class HomeNavbar extends React.Component {
                   Products
                 </Link>
               </Nav.Link>
-              <Nav.Link className="navbarBrand_order" href="#order" onClick={this.checkForLogIn}>
+              <Nav.Link
+                className="navbarBrand_order"
+                href="#order"
+                onClick={this.checkForLogIn}
+              >
                 {x}
-                  Order
-                
+                Order
               </Nav.Link>
-              {/* <InputGroup className="mb-3 input" inline> */}
-              {/* <InputGroup.Prepend className="searchIcon">
-                                
-                            <InputGroup.Text id="basic-addon1"><i class="fa fa-search" /></InputGroup.Text>
-                            </InputGroup.Prepend> */}
-              {/* <span className="searchIcon"><i class="fa fa-search" /></span>
-                            <FormControl className="input_formcontrol"
-                            id="search"
-                            placeholder="Search.."
-                            aria-label="Username"
-                            aria-describedby="basic-addon1"
-                            
-                            ></FormControl>
-
-                        </InputGroup> */}
-              <div class="form-group has-search">
+             
+              <div class="form-group has-search" style={{zIndex:"0"}}>
                 <span class="fa fa-search form-control-feedback"></span>
-                <input type="text" class="form-control" placeholder="Search" />
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Search"
+                  onChange={(e) => this.searchSpace(e)}
+                />
+                <div style={{zIndex:"1"}}> </div>
+                {/* */}
               </div>
 
               <Link className="cartlink" to="/Cart">
@@ -110,4 +188,14 @@ class HomeNavbar extends React.Component {
     );
   }
 }
-export default withRouter(HomeNavbar);
+const mapStateToProps = (state) => {
+  
+  return {
+    productData: state.productData
+  };
+};
+
+const mapDispatchToProps = {
+  getProducts
+};
+export default connect(mapStateToProps,mapDispatchToProps)(withRouter(HomeNavbar));
--- src/constants/BaseURL.js
+++ src/constants/BaseURL.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://2d8c9cedbbda.ngrok.io/";
+export const BASE_URL = "https://ebaa421524ec.ngrok.io/";
+// export const BASE_URL = "http://180.149.241.208:3023/";
